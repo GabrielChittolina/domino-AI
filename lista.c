f8fefffb7f11776b951f9c68db287648e59f42b2
MacPardo <marcos.adamczuk@gmail.com>
2016-09-13 18:06:06 -0300

    added function to lista.c

--- lista.c
+++ lista.c
@@ -7,3 +7,26 @@ typedef struct _pecaDomino{
 	struct _pecaDomino *right;
 	struct _pecaDomino *left;
 }TppecaDomino;
+
+TppecaDomino * new_list(){
+	return NULL;
+}
+
+TppecaDomino * add_left(TppecaDomino * list_a, 
+		int numberRight, int numberLeft){
+	TppecaDomino * new_piece = (TppecaDomino*)malloc(sizeof(TppecaDomino));
+	new_piece->numberRight = numberRight;
+	new_piece->numberLeft =  numberLeft;
+
+	if(list_a == NULL){
+		new_piece->right = NULL;
+		new_piece->left = NULL;
+		return new_piece;
+	}
+
+	new_piece->right = list_a;
+	list_a->left = new_piece;
+	new_piece->left = NULL;
+
+	return new_piece;
+}
