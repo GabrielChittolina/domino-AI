a0705a5ac480ede20f53512e4bedfed6ec972bd2
MacPardo <marcos.adamczuk@gmail.com>
2016-09-23 22:24:50 -0300

    headers

--- domino.h
+++ domino.h
@@ -3,4 +3,6 @@
 
 TppecaDomino * ai_move(TppecaDomino * deck_pc, TppecaDomino * deck_player, TppecaDomino ** _deck_table, TppecaDomino ** _deck_back);
 
+int can_play(TppecaDomino * deck_a, TppecaDomino * deck_table, TppecaDomino * deck_back);
+
 #endif
